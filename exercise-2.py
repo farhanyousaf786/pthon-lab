e7eb1f350ddd28c3708d030928ed69b942ad79d4
Farhan Yousaf <farhanyousaf1996@gmail.com>
2022-08-23 18:46:03 -0400

    ex#2 done

--- exercise-2.py
+++ exercise-2.py
@@ -6,3 +6,11 @@
 # 2. Print the following message:
 #      - What you entered is xx characters long
 # 3. Return to step 1, unless the word 'quit' was entered.
+
+
+userInput = input('Please enter a word or phrase: ').lower()
+
+while userInput.lower() != "quit":
+    result = len(userInput.replace(" ", ""))
+    print("What you entered is " + str(result)  + " characters long")
+    userInput = input('EPlease enter a word or phrase:').lower()
